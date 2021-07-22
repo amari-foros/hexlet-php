b58c8e60d022b56173903dfd428e69066a15633c
amari-foros <amari-foros@yandex.ru>
2021-07-22 13:39:43 +0300

    add diffirent in index.php

--- index.php
+++ index.php
@@ -2,3 +2,14 @@
 require_once __DIR__ . '/vendor/autoload.php';
 use Hexlet\Php\Runner;
 print_r(Runner\run());
+
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?>
+ </body>
+</html>
+
+php -S localhost:5555
